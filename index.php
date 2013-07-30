8f36fa9676940bbd0b931aac7cbcd199955ae313
Maarten Natzijl <info@yousoft.nl>
2013-07-30 10:37:59 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * DirectAdmin API implementation
+ *
+ * @author Maarten Natzijl, Yousoft Marketing & Internet <maarten@yousoft.nl>
+ * @version 0.1
+ * @link http://www.directadmin.com/api.html
+ *
+ */
+
+include 'lib/DirectAdmin/DirectAdmin.php';
+
+$da = new \DirectAdmin\DirectAdmin();
+$da->connect('domain.com', 2222);
+$da->set_login('username', 'password');
+
+$da->set_method('get');
+$da->query('/CMD_API_SHOW_USERS');
+$da = $da->fetch_parsed_body();
+
+//PRINT THE RESULT
+echo "<pre>";
+var_dump($da);
+echo "</pre>";
+
+?>
+
+
